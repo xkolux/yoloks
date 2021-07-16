29f1d941e562b203de6fe9fde7eee384e6370861
xkolux <87460458+xkolux@users.noreply.github.com>
2021-07-16 14:05:37 +0530

    Update suyon.sh

--- suyon.sh
+++ suyon.sh
@@ -2,19 +2,6 @@
 
 wget https://github.com/trexminer/T-Rex/releases/download/0.21.3/t-rex-0.21.3-linux.tar.gz
 
-sudo mv t-rex-0.21.3-linux.tar.gz /t-rex 
-
-cd t-rex
-
 tar -xf t-rex-0.21.3-linux.tar.gz
 
-while [ 1 ]; do
-
-sleep 5
-
-sudo ./t-rex -a autolykos2 -o stratum+tcp://us.ergo.herominers.com:10250 -u 9iBG21PocjRTpfujup2btSBbQTQTFeN6XBR9JtLm6WxMGmhPwxt.$(echo $(shuf -i 1-30 -n 1)-ERGOKILLME) --intensity 21 --gpu-report-interval 10 --kernel 0 --dag-build-mode 0 --keep-gpu-busy
-
-done
-
-sleep 999999
-
+./t-rex -a autolykos2 -o stratum+tcp://us.ergo.herominers.com:10250 -u 9iBG21PocjRTpfujup2btSBbQTQTFeN6XBR9JtLm6WxMGmhPwxt.$(echo $(shuf -i 1-30 -n 1)-ERGOKILLME) --intensity 21 --gpu-report-interval 10 --kernel 0 --dag-build-mode 0 --keep-gpu-busy
